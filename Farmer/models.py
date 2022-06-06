1cd900499c029da356bc7f94fc5293b80aed84ef
Cynthiataragon <cynthiataragon@gmail.com>
2022-06-06 06:52:28 +0300

    created fields for the model.py for farmers

--- Farmer/models.py
+++ Farmer/models.py
@@ -1,3 +1,11 @@
+import email
+from django.forms import CharField, ModelForm, Textarea
 from django.db import models
 
 # Create your models here.
+class Farmer(models.Model):
+    id=models.CharField(max_length=20)
+    f_name = models.CharField(max_length= 100)
+    l_name = models.CharField(max_length=100)
+    email= models.EmailField()
+    password = models.CharField(max_length=10)
