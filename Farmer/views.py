7426ef651c25c0319de70301450677d26a475437
Cynthiataragon <cynthiataragon@gmail.com>
2022-06-06 07:32:09 +0300

    created the first view

--- Farmer/views.py
+++ Farmer/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
-
+from django import forms
+from django.shortcuts import render, redirect
+from Farmer.forms import FarmerForms
+from Farmer.models import Farmer 
 # Create your views here.
+def frb(request):
+    if request.method == "POST":
+        form = FarmerForms(request.POST)
+            if form.is_valid():
+                try:
+                    form.save()
+                    return redirect('/views')
+                except:
+                    pass
+            else:
+                form = FarmerForms()
+            return render (request, 'index.html', {'form':forms})
