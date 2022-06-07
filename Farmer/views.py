4b56e97fab1f4d1d21ba49eef7393ff9c586593c
Cynthiataragon <cynthiataragon@gmail.com>
2022-06-07 07:44:52 +0300

    created functions for signup.signin, and signout

--- Farmer/views.py
+++ Farmer/views.py
@@ -5,3 +5,12 @@ from django.shortcuts import render
 def home(request):
     return HttpResponse("Hello world I am working")
 
+def signup(request):
+    return render(request, "Template/signup.html")
+
+def signin(request):
+    return render(request, "Template/signin.html")
+
+def signout(request):
+    return render(request, "Template/signout.html")
+    
