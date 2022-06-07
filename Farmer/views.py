5b313b5e8d5ad05cd6ef333b760c5f23db8dcda2
Cynthiataragon <cynthiataragon@gmail.com>
2022-06-07 07:32:27 +0300

    removed models py

--- Farmer/views.py
+++ Farmer/views.py
@@ -1,17 +1,7 @@
-from django import forms
-from django.shortcuts import render, redirect
-from Farmer.forms import FarmerForms
-from Farmer.models import Farmer 
+from django.http import HttpResponse
+from django.shortcuts import render
+
 # Create your views here.
-def frb(request):
-    if request.method == "POST":
-        form = FarmerForms(request.POST)
-            if form.is_valid():
-                try:
-                    form.save()
-                    return redirect('/views')
-                except:
-                    pass
-            else:
-                form = FarmerForms()
-            return render (request, 'index.html', {'form':forms})
+def home(request):
+    return HttpResponse("Hello world I am working")
+
